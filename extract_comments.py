e71beff5efdf54b69b0c94ab5157c697b67ab83c
Niklas Hofstetter <hofiniklas@gmail.com>
2026-02-09 13:38:59 +0100

    fixing

--- extract_comments.py
+++ extract_comments.py
@@ -403,8 +403,8 @@ single_patterns_de = [
 #input_path = "/pl/active/blast-data/corpora/reddit/subreddits24/changemyview_comments.zst"
 #output_path = "full_comments/changemyview.jsonl"
 
-input_path = "/pl/active/blast-data/corpora/reddit/subreddits24/AskAGerman_comments.zst"
-output_path = "full_comments/AskAGerman_comments.jsonl"
+input_path = "/pl/active/blast-data/corpora/reddit/subreddits24/DePi_comments.zst.zst"
+output_path = "full_comments/DePi_comments.jsonl"
 
 counter = 0
 matches = 0
